d324291d8f42b3424433774877c560aad3e38374
o.petrunko <itcoronos@gmail.com>
2015-12-18 19:41:22 +0200

    Добавление контроллов для работы с вк. Регистрация в вк

---
+++ OutputFaceBookData/VKEnterViewController.h
@@ -0,0 +1,13 @@
+//
+//  VKEnterViewController.h
+//  OutputFaceBookData
+//
+//  Created by Oleh Petrunko on 17.12.15.
+//  Copyright © 2015 Oleh Petrunko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VKEnterViewController : UIViewController
+
+@end
