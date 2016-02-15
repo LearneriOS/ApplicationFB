0deb2d4c47c49b284a3b94498a6a52694fedf3de
o.petrunko <itcoronos@gmail.com>
2016-02-15 23:03:02 +0300

    no message

---
+++ OutputFaceBookData/Consts.h
@@ -0,0 +1,16 @@
+//
+//  Consts.h
+//  OutputFaceBookData
+//
+//  Created by Oleh Petrunko on 09.02.16.
+//  Copyright © 2016 Oleh Petrunko. All rights reserved.
+//
+
+#ifndef Consts_h
+#define Consts_h
+
+static NSString *const kConstsAppId = @"5187957";
+static NSString *const kConstsSecret = @"sBsolEPAesngv7KAXDv7";
+static NSInteger const kConstsIndent = 20;
+
+#endif /* Consts_h */
